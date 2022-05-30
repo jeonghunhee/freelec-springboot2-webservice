f7dc9421577ac17db8c945031fa738b384f51dea
7gnsgml7@kakao.com <7gnsgml7@kakao.com>
2022-05-30 18:19:44 +0900

    Initial commit

---
+++ src/test/java/AWSBook/AWS/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package AWSBook.AWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
